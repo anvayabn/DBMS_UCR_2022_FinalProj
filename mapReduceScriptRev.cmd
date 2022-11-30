5f53179fd9b23ea1e708c7983c4b7f36e8e75605
anvayabn <65768145+anvayabn@users.noreply.github.com>
2022-11-30 14:39:56 -0800

    eddited filename

--- mapReduceScriptRev.cmd
+++ mapReduceScriptRev.cmd
@@ -15,13 +15,13 @@ call hadoop fs -mkdir /input_dir
 call hadoop fs -mkdir /output_dir
 call hadoop fs -mkdir /Results
 
-call hadoop fs -put %DATADIR%\WeatherStationLocations1.csv /input_dir 
+call hadoop fs -put %DATADIR%\WeatherStationLocations.csv /input_dir 
 call hadoop fs -put %DATADIR%\2006.txt /input_dir
 call hadoop fs -put %DATADIR%\2007.txt /input_dir
 call hadoop fs -put %DATADIR%\2008.txt /input_dir
 call hadoop fs -put %DATADIR%\2009.txt /input_dir
 
-call hadoop jar %DATADIR%\InputFileConstruction.jar InputFileConstruction /input_dir/WeatherStationLocations1.csv /input_dir/2006.txt /input_dir/2007.txt /input_dir/2008.txt /input_dir/2009.txt /output_dir
+call hadoop jar %DATADIR%\InputFileConstruction.jar InputFileConstruction /input_dir/WeatherStationLocations.csv /input_dir/2006.txt /input_dir/2007.txt /input_dir/2008.txt /input_dir/2009.txt /output_dir
 timeout /t 50 
 call hadoop fs -mv /output_dir/part-r-00000 /Results/InputFileConstruction.txt
 timeout /t 10 
